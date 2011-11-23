da351190a7237f3302fb13ff7402322bb70001a3
bmilesp <bmilesp@gmail.com>
2011-11-23 13:49:44 -0500

    SubDocumentBehavior.php created

---
+++ Model/Behavior/SubDocumentBehavior.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * SubDucoment behavior.
+ *
+ * Adds functionality specific to MongoDB/SubDucoment dbs
+ * Ability to create a model from a subdocument. This will add appropriate functionality to queries
+ * in order to achieve model functionalty on a subdocument
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2011, Brandon Plasters
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright     Copyright (c) 2011, Brandon Plasters
+ * @link          blog.brandonplasters.com
+ * @package       mongodb
+ * @subpackage    mongodb.models.behaviors
+ * @since         v 1.0 (24-May-2010)
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+/**
+ * SubDucomentBehavior class
+ *
+ * @uses          ModelBehavior
+ * @package       mongodb
+ * @subpackage    mongodb.models.behaviors
+ */
+class SubDucomentBehavior extends ModelBehavior {
+}
+	
